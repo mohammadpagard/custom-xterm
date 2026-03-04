d4e0750c16bd985b391eb0ef89cfd5f270f76481
richie <richiepagard@gmail.com>
2026-03-05 02:55:20 +0330

    feat; Implement `up` and `all` docker containers process functions.
    - Implement the `up-containers` funciton to show the UP containers in docker.
    
    - Implement the `all-container` function to show the ALL containers in docker.

--- docker_commands.sh
+++ docker_commands.sh
@@ -6,3 +6,17 @@
 green='\e[32m';
 blue='\e[34m';
 clean='\e[0m';
+
+DOCKER_ALL_CONTAINERS_ID=$(docker ps -aq);
+DOCKER_UP_CONTAINERS_ID=$(docker ps -q);
+
+function up_containers() {
+	echo "";
+	echo ${DOCKER_UP_CONTAINERS_ID};
+	echo "";
+}
+function all_containers() {
+	echo "";
+	echo ${DOCKER_ALL_CONTAINERS_ID};
+	echo "";
+}
