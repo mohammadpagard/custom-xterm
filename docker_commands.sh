aec5a4f29706724d8ee972f2c1e936588ba4091f
richie <richiepagard@gmail.com>
2026-03-05 03:02:18 +0330

    faat; Implement Colors functions (green and blue).

--- docker_commands.sh
+++ docker_commands.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 ##
-# Declare and initial colors
+# Declare and initial colors functions
 ##
 green='\e[32m';
 blue='\e[34m';
 clean='\e[0m';
 
+function ColorGreen() {
+	echo -ne "${green}$1${clean}";
+}
+function ColorBlue() {
+	echo -ne "${green}$1${clean}";
+}
+
+##
+# Declare and implement the Docker
+# everyday commands.
+##
 DOCKER_ALL_CONTAINERS_ID=$(docker ps -aq);
 DOCKER_UP_CONTAINERS_ID=$(docker ps -q);
 
