015921698f5d4f3c26b11f59a0d472719066c4a7
richie <richiepagard@gmail.com>
2026-03-05 13:07:27 +0330

    feat; Implement a function to stop all containers.
    - Remove the `up-containers` function.
    
    - Implement a new function to stop all containers.
    
    - Add the `stop-containers` function to the main menu.

--- docker_commands.sh
+++ docker_commands.sh
@@ -21,12 +21,6 @@ function ColorBlue() {
 DOCKER_ALL_CONTAINERS_ID=$(docker ps -aq);
 DOCKER_UP_CONTAINERS_ID=$(docker ps -q);
 
-function up_containers_id() {
-	echo "";
-	echo ${DOCKER_UP_CONTAINERS_ID};
-	echo "------------";
-	echo "";
-}
 function all_containers_id() {
 	echo "";
 	echo ${DOCKER_ALL_CONTAINERS_ID};
@@ -39,6 +33,13 @@ function show_containers_json() {
 	printf "%s" ${command_format} | jq
 	echo "";
 }
+function stop_containers() {
+	echo "";
+	stop_command=$(docker stop ${DOCKER_UP_CONTAINERS_ID});
+	echo ${stop_command};
+	echo "------------";
+	echo "";
+}
 
 
 ##
@@ -47,18 +48,18 @@ function show_containers_json() {
 function menu() {
 	echo -ne "
 	$(ColorBlue '<< Richie Docker Commands Boilerplate >>')
-	$(ColorGreen '1)') Show Only UP Containers ID
-	$(ColorGreen '2)') Show All The Containers ID
-	$(ColorGreen '3)') Show UP Containers In JSON Format
+	$(ColorGreen '1)') Show All The Containers ID
+	$(ColorGreen '2)') Show UP Containers In JSON Format
+	$(ColorGreen '3)') Stop All Containers
 	$(ColorGreen '0)') Exit
 
 	Choose one: "
 
 	read user_choice;
 	case ${user_choice} in
-		1) up_containers_id; menu;;
-		2) all_containers_id; menu;;
-		3) show_containers_json; menu;;
+		1) all_containers_id; menu;;
+		2) show_containers_json; menu;;
+		3) stop_containers; menu;;
 		0) exit 0 ;;
 	esac
 }
